ea7a3911151adb77089c82bb6539d124fd46bad9
David Lim <david.lim@berkeley.edu>
2025-01-26 22:43:41 -0800

    Init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>My Name is David</div>
+      <div>New Project yay</div>
+    </>
+  );
+}
+
+export default App;
