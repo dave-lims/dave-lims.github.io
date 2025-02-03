03acf9ca3f5592a70967bee4a64645cb640c34e4
David Lim <david.lim@berkeley.edu>
2025-02-02 22:15:59 -0800

    MVP maintenance page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// import { useState } from "react";
 import me from "./assets/me.svg";
 
 function App() {
@@ -15,8 +14,8 @@ function App() {
       <br />
 
       <p>
-        🚧 This site is currently under development. 🚧 <br /> Please check back
-        later and get excited for changes that are coming!
+        🚧 <br /> This site is currently under development. <br /> Please check
+        back later and get excited for changes that are coming!
       </p>
 
       <br />
