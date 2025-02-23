b57adada68c2cddfc9cb4973bdf8fea3a4978b98
David Lim <david.lim@berkeley.edu>
2025-02-22 17:13:40 -0800

    RESET

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,1 @@
 // Filler file for ✨ prettier ✨ imports
-import Button from "./Button/Button"
-import Card from "./Card/Card";
-import ProjectCard from "./ProjectCard/ProjectCard";
-import Tag from "./Tag/Tag";
-import HomeNav from "./HomeNav/HomeNav";
-
-export { Button, Card, ProjectCard, Tag, HomeNav };
